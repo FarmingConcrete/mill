b7a44fe280b97e574ead21d57379e301131d74f8
Eric Brelsford <ebrelsford@gmail.com>
2014-11-03 23:49:30 -0500

    Charts for everyone

--- js/view.js
+++ js/view.js
@@ -1,27 +1,34 @@
-var dataSet = [];
+function createFakeDataset() {
+    var dataSet = [],
+        currentDate = moment('2014-01-01'),
+        crops = ['corn', 'peppers (hot)', 'tomatoes'];
 
-var currentDate = moment('2014-01-01'),
-    crops = ['corn', 'peppers (hot)', 'tomatoes'];
-
-_.each(_.range(_.random(75)), function () {
-    currentDate = currentDate.add(_.random(3), 'days');
-    dataSet.push([
-        currentDate.format('M/D/YY'),
-        _.random(5),
-        _.sample(crops)
-    ]);
-});
+    _.each(_.range(_.random(75)), function () {
+        currentDate = currentDate.add(_.random(3), 'days');
+        dataSet.push([
+            currentDate.format('M/D/YY'),
+            _.random(5),
+            _.sample(crops)
+        ]);
+    });
+    return dataSet;
+}
 
 $(document).ready(function () {
-    $('.metric-table').dataTable({
-        columns: [
+    $('.metric-table').each(function () {
+        var columns = [
             { "title": "Recorded" },
-            { "title": "Plants" },
-            { "title": "Crop" }
-        ],
-        data: dataSet,
-        lengthChange: false,
-        pageLength: 5,
-        searching: false
+            { "title": $(this).data('number-label') }
+        ];
+        if ($(this).data('crops')) {
+            columns.push({ "title": "Crop" });
+        }
+        $(this).dataTable({
+            columns: columns,
+            data: createFakeDataset(),
+            lengthChange: false,
+            pageLength: 5,
+            searching: false
+        });
     });
 });
